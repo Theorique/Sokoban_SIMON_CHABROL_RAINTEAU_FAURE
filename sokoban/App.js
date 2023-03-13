4a9908f2511fba9682312bf9e121d23e3f3b5c38
bachabrol <chabrol96@gmail.com>
2023-03-13 16:35:21 +0100

    commit

--- sokoban/App.js
+++ sokoban/App.js
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import './node_modules/bootstrap/dist/css/bootstrap.css';
+import UploadFiles from "./composants/UploadFiles";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View className="App">
+        <UploadFiles />
+      </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
