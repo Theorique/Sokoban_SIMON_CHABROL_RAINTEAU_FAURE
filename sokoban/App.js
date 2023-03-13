11906a65135442929bec8b15033250fa18c4fec7
HugoHysequa <hugo.hysequa@gmail.com>
2023-03-13 14:35:29 +0100

    Fetch du json en dur

--- sokoban/App.js
+++ sokoban/App.js
@@ -1,19 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { plateau: ['ok'] };
+  }
+  
+  componentDidMount() {
+    const customData = require('./plateau/plateau-dur.json');
+    console.log(customData);
+    this.setState( {plateau: customData});
+  }
+  render() {
+    return (
+      <View>
+        <Text>{this.state.plateau["text"]}</Text>
+      <FlatList
+        data={[1, 2, 3, 4]}
+        numColumns={3}
+        renderItem={({ item }) => <Text>{item}</Text>}
+      />
+      </View>
+    );
+  }
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding:100,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
